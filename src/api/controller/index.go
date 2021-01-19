44aeb86c19be895e111bc19e390c2ca71bff21d4
Hyuma     Asada <h.asada@gemcook.com>
2021-01-19 16:23:07 +0900

    スクレイピングし、配列で表示させる

--- src/api/controller/index.go
+++ src/api/controller/index.go
@@ -1,37 +1,120 @@
 package controller
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/sclevine/agouti"
+	"github.com/gin-gonic/gin"
+	"github.com/sclevine/agouti"
+	"log"
 )
 
-func IndexDisplayAction(c *gin.Context){
-    c.HTML(200, "index.html", gin.H{"message": "hello gin"})
+func IndexDisplayAction(c *gin.Context) {
+	c.HTML(200, "index.html", gin.H{"message": "hello gin"})
 }
 
-func AnytimeDisplayAction(c *gin.Context){
-agoutiDriver := agouti.ChromeDriver()
-    agoutiDriver.Start()
-    defer agoutiDriver.Stop()
-    page, _ := agoutiDriver.NewPage()
-    
-    
-    // 六町店
-   page.Navigate("https://www.anytimefitness.co.jp/rokucho/"); 
-   title1, _ := page.Title()
-   info1, _ := page.FindByID("info-box").Text()
+func chrome() *agouti.Page {
+	agoutiDriver := agouti.ChromeDriver(
+		agouti.ChromeOptions("args", []string{
+			"--headless", // headlessモードの指定 サイト読み込み時の起動をなくす
+		}),
+	)
+	agoutiDriver.Start()
+	// defer agoutiDriver.Stop()
+	page, _ := agoutiDriver.NewPage()
+	return page
+}
 
-   // 新柴又店
-   page.Navigate("https://www.anytimefitness.co.jp/n-shibamata/"); 
-   title2, _ := page.Title()
-   info2, _ := page.FindByID("info-box").Text()
+func AnytimeDisplayAction(c *gin.Context) {
+
+	messages := []string{}
+    test := make(chan []string)
+
+	go func(){
+		page := chrome()
+
+		// 六町店
+		page.Navigate("https://www.anytimefitness.co.jp/rokucho/")
+		title, _ := page.Title()
+		info, _ := page.FindByID("info-box").Text()
+		
+		y := []string{}
+		y = append(y, title,info)
+		log.Println(1)
+		
+		test <- y
+	}()
+
+	go func() {
+		page := chrome()
+
+		// 新柴又店
+		page.Navigate("https://www.anytimefitness.co.jp/n-shibamata/")
+
+		title, _ := page.Title()
+		info, _ := page.FindByID("info-box").Text()
+
+		y := []string{}
+		y = append(y, title,info)
+		log.Println(1)
+
+		test <- y
+	}()
+
+	go func() {
+		page := chrome()
+
+		// 曙橋駅店
+		page.Navigate("https://www.anytimefitness.co.jp/akebonobashi/")
+
+		title, _ := page.Title()
+		info, _ := page.FindByID("info-box").Text()
+
+		y := []string{}
+		y = append(y, title,info)
+		log.Println(1)
+
+		test <- y
+	}()
 
 
-    c.HTML(200, "anytime.html", gin.H{
-        "message": "エニタイム",
-        "title1": title1,
-        "info1": info1,
-        "title2": title2,
-        "info2": info2,
-    })
+	go func() {
+		page := chrome()
+
+		// 新中野店
+		page.Navigate("https://www.anytimefitness.co.jp/shinnakano/")
+
+		title, _ := page.Title()
+		info, _ := page.FindByID("info-box").Text()
+
+		y := []string{}
+		y = append(y, title,info)
+		log.Println(1)
+
+		test <- y
+	}()
+
+	go func() {
+		page := chrome()
+
+		// 落合店
+		page.Navigate("https://www.anytimefitness.co.jp/ochiai/")
+
+		title, _ := page.Title()
+		info, _ := page.FindByID("info-box").Text()
+
+		y := []string{}
+		y = append(y, title,info)
+		log.Println(1)
+
+		test <- y
+	}()
+
+	messages = append(messages, <-test...)
+    messages = append(messages, <-test...)
+	messages = append(messages, <-test...)
+	messages = append(messages, <-test...)
+	messages = append(messages, <-test...)
+
+	c.HTML(200, "anytime.html", gin.H{
+		"message": "エニタイム",
+		"titles":  messages,
+	})
 }
