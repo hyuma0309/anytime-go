ee7b9e73fbdf7c4c549fdc705a33597ed2155286
Hyuma     Asada <h.asada@gemcook.com>
2021-01-26 13:40:19 +0900

    価格が安い順で表示されるように変更

--- src/api/controller/index.go
+++ src/api/controller/index.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sclevine/agouti"
-	"log"
 	"net/url"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func IndexDisplayAction(c *gin.Context) {
@@ -15,9 +19,9 @@ func chrome() *agouti.Page {
 	agoutiDriver := agouti.ChromeDriver(
 		agouti.ChromeOptions("args", []string{
 			"--headless", // headlessモードの指定 サイト読み込み時の起動をなくす
-			"--no-sandbox", 
-			"--disable-gpu", // アクセラレーションの排除　ちらつき解消
-            "--disable-dev-shm-usage", //chromeが破損しないようDockerのメモリスペースを大きいものに変更
+			"--no-sandbox",
+			"--disable-gpu",           // アクセラレーションの排除　ちらつき解消
+			"--disable-dev-shm-usage", //chromeが破損しないようDockerのメモリスペースを大きいものに変更
 		}),
 	)
 	agoutiDriver.Start()
@@ -25,120 +29,225 @@ func chrome() *agouti.Page {
 	page, _ := agoutiDriver.NewPage()
 	return page
 }
+
 type anytime struct {
-  Title string
-  Price string
-  Url *url.URL
+	Title string
+	Price int
+	Info  string
+	Url   *url.URL
 }
 
-func NewAnytime(title string, price string, url *url.URL) anytime {
-    return anytime{Title: title, Price: price, Url: url}
+type Lists []anytime
+
+func (l Lists) Len() int {
+	// Len is the number of elements in the collection.
+	return len(l)
+}
+
+func (l Lists) Less(i, j int) bool {
+	// Less reports whether the element with
+	// index i should sort before the element with index j.
+	return l[i].Price < l[j].Price
+	// return i < j ← これだとうまく動かない
+}
+
+func (l Lists) Swap(i, j int) {
+	// Swap swaps the elements with indexes i and j.
+	l[i], l[j] = l[j], l[i]
+}
+
+func NewAnytime(title string, price int, info string, url *url.URL) anytime {
+	return anytime{Title: title, Price: price, Info: info, Url: url}
 }
 
 func AnytimeDisplayAction(c *gin.Context) {
-	var messages []interface{}
-	ch := make(chan []interface{})
+	// var messages []interface{}
+	ch := make(chan []anytime)
 
-	go func(){
+	go func() {
 		page := chrome()
-		link := "https://www.anytimefitness.co.jp/rokucho/"
+		link := "https://www.anytimefitness.co.jp/ebisu/"
 
-		// 六町店
+		// 恵比寿店
 		page.Navigate(link)
 		title, _ := page.Title()
 		priceClass, _ := page.FindByClass("price").Text()
-		price := string([]rune(priceClass)[:23])
+		priceString := string([]rune(priceClass)[:23])
+
+		re := regexp.MustCompile("(..[0-9]+)[^0-9]*$")
+		result := re.FindStringSubmatch(priceString)
+		if result == nil {
+			fmt.Println("数字なし")
+		}
+		result[1] = strings.Replace(result[1], ",", "", 1)
+		price, _ := strconv.Atoi(result[1])
+
 		url, _ := url.Parse(link)
+		info, _ := page.Find("#campaign > div > div > p.fl").Text()
+		info = info[:strings.Index(info, "※")]
 
-		
-		var y []interface{}
-		y = append(y, NewAnytime(title,price,url))
+		var y []anytime
+		y = append(y, NewAnytime(title, price, info, url))
 
-		log.Println(y)
+		// log.Println(y)
 		ch <- y
 	}()
 
 	go func() {
 		page := chrome()
-		link := "https://www.anytimefitness.co.jp/n-shibamata/"
+		link := "https://www.anytimefitness.co.jp/keiosasazuka/"
 
-		// 新柴又店
+		// 京王笹塚店
 		page.Navigate(link)
 
 		title, _ := page.Title()
 		priceClass, _ := page.FindByClass("price").Text()
-		price := string([]rune(priceClass)[:23])
+		priceString := string([]rune(priceClass)[:23])
+
+		re := regexp.MustCompile("(..[0-9]+)[^0-9]*$")
+		result := re.FindStringSubmatch(priceString)
+		if result == nil {
+			fmt.Println("数字なし")
+		}
+		result[1] = strings.Replace(result[1], ",", "", 1)
+		price, _ := strconv.Atoi(result[1])
+
 		url, _ := url.Parse(link)
-		
-		var y []interface{}
-		y = append(y, NewAnytime(title,price,url))
+		info, _ := page.Find("#campaign > div > div > p.fl").Text()
+		info = info[:strings.Index(info, "※")]
 
-		ch <- y
-	}()
-
-	go func() {
-		page := chrome()
-		link := "https://www.anytimefitness.co.jp/akebonobashi/"
-
-		// 曙橋駅店
-		page.Navigate(link)
-
-		title, _ := page.Title()
-		priceClass, _ := page.FindByClass("price").Text()
-		price := string([]rune(priceClass)[:23])
-		url, _ := url.Parse(link)
-		
-		var y []interface{}
-		y = append(y, NewAnytime(title,price,url))
-
-		ch <- y
-	}()
-
-
-	go func() {
-		page := chrome()
-		link := "https://www.anytimefitness.co.jp/shinnakano/"
-
-		// 新中野店
-		page.Navigate(link)
-
-		title, _ := page.Title()
-		priceClass, _ := page.FindByClass("price").Text()
-		price := string([]rune(priceClass)[:23])
-		url, _ := url.Parse(link)
-		
-		var y []interface{}
-		y = append(y, NewAnytime(title,price,url))
+		var y []anytime
+		y = append(y, NewAnytime(title, price, info, url))
 
 		ch <- y
 	}()
 
 	go func() {
 		page := chrome()
-		link := "https://www.anytimefitness.co.jp/ochiai/"
+		link := "https://www.anytimefitness.co.jp/hatsudai/"
 
-		// 落合店
+		// 渋谷初台店
 		page.Navigate(link)
 
 		title, _ := page.Title()
 		priceClass, _ := page.FindByClass("price").Text()
-		price := string([]rune(priceClass)[:23])
+		priceString := string([]rune(priceClass)[:23])
+
+		re := regexp.MustCompile("(..[0-9]+)[^0-9]*$")
+		result := re.FindStringSubmatch(priceString)
+		if result == nil {
+			fmt.Println("数字なし")
+		}
+		result[1] = strings.Replace(result[1], ",", "", 1)
+		price, _ := strconv.Atoi(result[1])
+
 		url, _ := url.Parse(link)
-		
-		var y []interface{}
-		y = append(y, NewAnytime(title,price,url))
+		info := "キャンペーン情報はありません"
+
+		var y []anytime
+		y = append(y, NewAnytime(title, price, info, url))
 
 		ch <- y
 	}()
 
-	messages = append(messages, <-ch...)
-	messages = append(messages, <-ch...)
-	messages = append(messages, <-ch...)
-	messages = append(messages, <-ch...)
-	messages = append(messages, <-ch...)
+	go func() {
+		page := chrome()
+		link := "https://www.anytimefitness.co.jp/hiroohsf/"
+
+		// 広尾高校前店
+		page.Navigate(link)
+
+		title, _ := page.Title()
+		priceClass, _ := page.FindByClass("price").Text()
+		priceString := string([]rune(priceClass)[:23])
+
+		re := regexp.MustCompile("(..[0-9]+)[^0-9]*$")
+		result := re.FindStringSubmatch(priceString)
+		if result == nil {
+			fmt.Println("数字なし")
+		}
+		result[1] = strings.Replace(result[1], ",", "", 1)
+		price, _ := strconv.Atoi(result[1])
+
+		url, _ := url.Parse(link)
+		info, _ := page.Find("#campaign > div > div > p.fl").Text()
+		info = info[:strings.Index(info, "※")]
+
+		var y []anytime
+		y = append(y, NewAnytime(title, price, info, url))
+
+		ch <- y
+	}()
+
+	go func() {
+		page := chrome()
+		link := "https://www.anytimefitness.co.jp/yoyogi/"
+
+		// 元代々木店
+		page.Navigate(link)
+
+		title, _ := page.Title()
+		priceClass, _ := page.FindByClass("price").Text()
+		priceString := string([]rune(priceClass)[:23])
+
+		re := regexp.MustCompile("(..[0-9]+)[^0-9]*$")
+		result := re.FindStringSubmatch(priceString)
+		if result == nil {
+			fmt.Println("数字なし")
+		}
+		result[1] = strings.Replace(result[1], ",", "", 1)
+		price, _ := strconv.Atoi(result[1])
+
+		url, _ := url.Parse(link)
+		info, _ := page.Find("#campaign > div > div > p.fl").Text()
+		info = info[:strings.Index(info, "※")]
+
+		var y []anytime
+		y = append(y, NewAnytime(title, price, info, url))
+
+		ch <- y
+	}()
+
+	go func() {
+		page := chrome()
+		link := "https://www.anytimefitness.co.jp/yoyogi-sendagaya/"
+
+		// 代々木店
+		page.Navigate(link)
+
+		title, _ := page.Title()
+		priceClass, _ := page.FindByClass("price").Text()
+		priceString := string([]rune(priceClass)[:23])
+
+		re := regexp.MustCompile("(..[0-9]+)[^0-9]*$")
+		result := re.FindStringSubmatch(priceString)
+		if result == nil {
+			fmt.Println("数字なし")
+		}
+		result[1] = strings.Replace(result[1], ",", "", 1)
+		price, _ := strconv.Atoi(result[1])
+
+		url, _ := url.Parse(link)
+		info := "キャンペーン情報はありません"
+
+		var y []anytime
+		y = append(y, NewAnytime(title, price, info, url))
+
+		ch <- y
+	}()
+
+	lists := Lists{}
+	lists = append(lists, <-ch...)
+	lists = append(lists, <-ch...)
+	lists = append(lists, <-ch...)
+	lists = append(lists, <-ch...)
+	lists = append(lists, <-ch...)
+	lists = append(lists, <-ch...)
+
+	sort.Sort(lists)
 
 	c.HTML(200, "anytime.html", gin.H{
 		"message": "エニタイム",
-		"titles":  messages,
+		"titles":  lists,
 	})
 }
